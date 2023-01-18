3d0892c5b7cfa789e6aa49e4ab9342fa0f838e2d
Julian Louis <contact@julianlouis.fr>
2023-01-18 23:29:41 +0100

    Fix google fonts icons import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0&display=optional" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
       </Head>
       <body>
         <Main />
