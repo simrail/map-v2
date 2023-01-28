e4719e9f5ad9a3170e5fde3a0e50adf4c6e90db0
Julian Louis <contact@julianlouis.fr>
2023-01-29 00:26:53 +0100

    test

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.prepare().then(() => {
             const parsedUrl = parse(req.url, true)
             const { pathname, query } = parsedUrl
 
-            console.log('debug', req.host, pathname, req.url, req.headers.host)
-            if (req.host === 'map.simrail.app' && pathname === '/') {
+            if (req.headers.host === 'map.simrail.app' && pathname === '/') {
                 await app.render(req, res, '/servers', query)
             } else {
                 await handle(req, res, parsedUrl)
