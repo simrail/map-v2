ea1f34c30e52cf7a1b47b9db0c672e714888f3da
Julian Louis <contact@julianlouis.fr>
2023-02-09 21:15:07 +0100

    Add meta desc, train search system, and edit logos

---
+++ components/SearchInput.tsx
@@ -0,0 +1,52 @@
+import { Center, Flex, Space } from '@mantine/core';
+import { Train } from '@simrail/types';
+import { useSelectedTrain } from 'contexts/SelectedTrainContext';
+import Script from 'next/script';
+import { useState } from 'react';
+import { useMap } from 'react-leaflet';
+import style from '../styles/SearchInput.module.css';
+
+
+type SearchInputProps = {
+    trains: Train[]
+}
+export const SearchInput = ({ trains }: SearchInputProps) => {
+
+    const [searchInput, setSearch] = useState('');
+    const { selectedTrain, setSelectedTrain } = useSelectedTrain()
+    const map = useMap();
+
+    let searchedTrains = trains.filter((train) => train.TrainNoLocal.startsWith(searchInput)).slice(0, 5)
+
+
+    return <div className={style.wrapper}>
+        <div className={style.container}>
+            <div className={style.content}>
+                <div className={style.inputBox}>
+                    <input className={style.input} type="text" value={searchInput} placeholder="Saisissez le numéro d'un train"
+                        onKeyDown={(event) => {
+                            if (event.code === 'Enter') {
+                                setSelectedTrain(searchedTrains[0])
+                                setSearch('')
+                                map?.setZoom(13)
+
+                            }
+                        }}
+                        onChange={(event) => setSearch(event.target.value)} />
+                </div>
+                <ul className={style.list}>
+                    {searchInput && searchedTrains.map(train =>
+
+                        <li className={style.item} onClick={() => {
+                            setSearch('')
+                            setSelectedTrain(train)
+                            map?.setZoom(13)
+
+                        }}>{train.TrainNoLocal} - {train.TrainName}</li>
+                    )}
+                </ul>
+            </div>
+        </div>
+
+    </div >
+}
