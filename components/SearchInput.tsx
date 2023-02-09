a793521f9633ad2c7803282f924633f291edb9b7
Julian Louis <contact@julianlouis.fr>
2023-02-09 21:34:18 +0100

    translate

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -23,7 +23,7 @@ export const SearchInput = ({ trains }: SearchInputProps) => {
         <div className={style.container}>
             <div className={style.content}>
                 <div className={style.inputBox}>
-                    <input className={style.input} type="text" value={searchInput} placeholder="Saisissez le numéro d'un train"
+                    <input className={style.input} type="text" value={searchInput} placeholder="Select your train number"
                         onKeyDown={(event) => {
                             if (event.code === 'Enter') {
                                 setSelectedTrain(searchedTrains[0])
