7013de6456f1715b28d3cae49ba846fc70968b12
Julian <github@julianlouis.fr>
2023-07-05 17:10:02 +0200

    fix image

--- components/steam.ts
+++ components/steam.ts
@@ -1,12 +1,12 @@
-import {ProfileResponse} from "../pages/api/profile";
+import { ProfileResponse } from "../pages/api/profile";
 
 const getSteamProfileInfos = (steamId: string): Promise<ProfileResponse> =>
-    fetch('/api/profile?steamid=' + steamId).then((r) => r.json());
+    fetch('https://simrail-edr.emeraldnetwork.xyz/steam/' + steamId).then((r) => r.json());
 
 export async function getSteamProfileOrBot(steamId: string | null | undefined) {
     if (steamId)
         return getSteamProfileInfos(steamId)
-            .then((profile) => [profile.avatarUrl, profile.username])
+            .then((profile) => [profile.avatar, profile.personaname])
     else
         return Promise.resolve([null, "BOT"]);
 }
