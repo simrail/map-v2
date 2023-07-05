32a33fe29bbb030a37e3748d4395e1cf577531df
Julian <contact@julianlouis.fr>
2023-07-05 16:55:46 +0200

    Update profile.ts

--- pages/api/profile.ts
+++ pages/api/profile.ts
@@ -24,20 +24,26 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<ProfileResponse | { error: true }>
 ) {
-    const { steamid } = req.query
 
-    if (!steamid) return res.status(400).json({ error: true });
-
-    let data = await fetch("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + process.env.STEAM_TOKEN + "&steamids=" + steamid)
-    let steamProfileResponse: SteamProfileResponse = await data.json()
-
-
-    if (!steamProfileResponse.response.players[0]) return res.status(400).json({ error: true });
-
-    let avatar = steamProfileResponse.response.players[0].avatarmedium
-    let username = steamProfileResponse.response.players[0].personaname
-
-
-    res.setHeader('Cache-Control', 'public, s-maxage=86400');
-    res.status(200).json({ avatarUrl: avatar, username: username })
+    try {
+        const { steamid } = req.query
+    
+        if (!steamid) return res.status(400).json({ error: true });
+    
+        
+        let data = await fetch("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + process.env.STEAM_TOKEN + "&steamids=" + steamid)
+        let steamProfileResponse: SteamProfileResponse = await data.json()
+    
+    
+        if (!steamProfileResponse.response.players[0]) return res.status(400).json({ error: true });
+    
+        let avatar = steamProfileResponse.response.players[0].avatarmedium
+        let username = steamProfileResponse.response.players[0].personaname
+    
+    
+        res.setHeader('Cache-Control', 'public, s-maxage=86400');
+        res.status(200).json({ avatarUrl: avatar, username: username })
+    } catch (e) {
+        return console.error(e); // error in the above string (in this case, yes)!
+    }
 }
