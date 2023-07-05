3a743c7f7fa3ac72ca583f77f952a5b6595332df
Julian <github@julianlouis.fr>
2023-07-05 17:10:20 +0200

    fix

--- pages/api/profile.ts
+++ pages/api/profile.ts
@@ -16,8 +16,8 @@ type SteamProfilePlayer = {
 }
 
 type ProfileResponse = {
-    avatarUrl: string,
-    username: string,
+    avatar: string,
+    personaname: string,
 }
 export type { ProfileResponse }
 export default async function handler(
@@ -27,10 +27,10 @@ export default async function handler(
 
     try {
         const { steamid } = req.query
-    
+
         if (!steamid) return res.status(400).json({ error: true });
-    
-        
+
+
         let data = await fetch("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + process.env.STEAM_TOKEN + "&steamids=" + steamid)
         let errorCode = data.ok ? false : data.status
         if (errorCode) {
@@ -38,14 +38,14 @@ export default async function handler(
             res.status(500).json({ error: true });
         }
         let steamProfileResponse: SteamProfileResponse = await data.json()
-    
-    
+
+
         if (!steamProfileResponse.response.players[0]) return res.status(400).json({ error: true });
-    
+
         let avatar = steamProfileResponse.response.players[0].avatarmedium
         let username = steamProfileResponse.response.players[0].personaname
-    
-    
+
+
         res.setHeader('Cache-Control', 'public, s-maxage=86400');
         res.status(200).json({ avatarUrl: avatar, username: username })
     } catch (e) {
