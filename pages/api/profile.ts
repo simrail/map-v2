2711cf32614d3d21e6b980160504fcb8ad7f874e
Julian <contact@julianlouis.fr>
2023-07-05 16:59:53 +0200

    Update profile.ts

--- pages/api/profile.ts
+++ pages/api/profile.ts
@@ -32,7 +32,7 @@ export default async function handler(
     
         
         let data = await fetch("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + process.env.STEAM_TOKEN + "&steamids=" + steamid)
-        let errorCode = result.ok ? false : result.statusCode
+        let errorCode = data.ok ? false : data.statusCode
         if (errorCode) {
             console.log(result, errorCode)
             res.status(500).json({ error: true });
