6cdef30f3dbcabed96fb75c9e0fa5c4966749af5
SparklesReal <jultomtenriktig@gmail.com>
2024-05-04 08:08:08 +0200

    Lists all types if they are diffrent

--- components/TrainText.tsx
+++ components/TrainText.tsx
@@ -10,13 +10,15 @@ type TrainTextProps = {
 
 const TrainText = ({ train, username }: TrainTextProps) => {
 
-    let trainType = train.Vehicles[0];
+    let originTrainType = train.Vehicles[0];
+    let trainTypes = [];
     let trainAmount = 1;
+    let totalTrainAmount = 1;
     let trainImages = [];
     trainImages.push(<><Image src={getTrainImagePath(train.Vehicles[0])} width={"200"} height={"85"} alt={train.Vehicles[0]} /><br /></>); // first train image
     for (let i = 1; i < train.Vehicles.length; i++) {
-        if (train.Vehicles[i] === trainType || train.Vehicles[i].split("/")[0] == trainType.split("/")[0]) { // Loco num can vary hence the before /
-            trainAmount++;
+        if (train.Vehicles[i] === originTrainType) {
+            trainAmount++, totalTrainAmount++;
             trainImages.push(<><Image src={getTrainImagePath(train.Vehicles[i])} width={"200"} height={"85"} alt={train.Vehicles[i]} /><br /></>);
         }
     }
@@ -24,8 +26,25 @@ const TrainText = ({ train, username }: TrainTextProps) => {
     let trainAmountString = '';
     if (trainAmount > 1) 
         trainAmountString = `x${trainAmount}`;
+    trainTypes.push(<>Type: {train.Vehicles[0]} {trainAmountString}<br /></>)
 
-    let wagonsAmount = train.Vehicles.length - trainAmount;
+    for (let i = 1; i < train.Vehicles.length; i++) {
+        trainAmount = 0;
+        if (train.Vehicles[i].split("/")[0] == originTrainType.split("/")[0] && train.Vehicles[i] != originTrainType) { // If  it is a diffrent subversion of train type
+            for (let j = 0; j < train.Vehicles.length; j++) {
+                if (train.Vehicles[j] === train.Vehicles[i]) 
+                    trainAmount++, totalTrainAmount++;
+            }
+            trainAmountString = '';
+            if (trainAmount > 1) 
+                trainAmountString = `x${trainAmount}`;
+            trainTypes.push(<>Type: {train.Vehicles[i]} {trainAmountString}<br /></>)
+            trainImages.push(<><Image src={getTrainImagePath(train.Vehicles[i])} width={"200"} height={"85"} alt={"Failed to load " + train.Vehicles[i]} /><br /></>);
+            totalTrainAmount++;
+        }
+    }
+
+    let wagonsAmount = train.Vehicles.length - totalTrainAmount;
     let wagonsAmountString = ''
     if (wagonsAmount > 0 && train.Vehicles[0].includes('EN57') == false)
         wagonsAmountString = <>Wagons: x{wagonsAmount} <br /></>;
@@ -37,7 +56,7 @@ const TrainText = ({ train, username }: TrainTextProps) => {
     return (
         <>
             {trainImages}
-            Type: {train.Vehicles[0]} {trainAmountString}<br />
+            {trainTypes}
             Train: {train.TrainName} {train.TrainNoLocal}<br />
             {wagonsAmountString}
             User: {username}<br />
