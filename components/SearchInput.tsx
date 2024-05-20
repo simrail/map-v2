ff25bb4357f17f4db9f43cc083c05f6c44fe9f23
SparklesReal <jultomtenriktig@gmail.com>
2024-05-20 13:59:05 +0200

    More dark mode

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -16,14 +16,32 @@ export const SearchInput = ({ trains }: SearchInputProps) => {
     const { selectedTrain, setSelectedTrain } = useSelectedTrain()
     const map = useMap();
 
+    // This is prob stupid
+    const theme = localStorage.getItem('theme');
+    var inputBoxTheme
+    var inputTheme
+    var listTheme
+    switch(theme) {
+        case 'dark':
+            inputBoxTheme = style.inputBoxDark
+            inputTheme = style.inputDark
+            listTheme = style.listDark
+        break;
+        case 'light':
+            inputBoxTheme = style.inputBoxLight
+            inputTheme = style.inputLight
+            listTheme = style.listLight
+        break;
+    }
+
     let searchedTrains = trains.filter((train) => train.TrainNoLocal.startsWith(searchInput)).slice(0, 5)
 
 
     return <div className={style.wrapper}>
         <div className={style.container}>
             <div className={style.content}>
-                <div className={style.inputBox}>
-                    <input className={style.input} type="text" value={searchInput} placeholder="Enter your train number"
+                <div className={inputBoxTheme}>
+                    <input className={inputTheme} type="text" value={searchInput} placeholder="Enter your train number"
                         onKeyDown={(event) => {
                             if (event.code === 'Enter') {
                                 setSelectedTrain(searchedTrains[0])
@@ -34,7 +52,7 @@ export const SearchInput = ({ trains }: SearchInputProps) => {
                         }}
                         onChange={(event) => setSearch(event.target.value)} />
                 </div>
-                <ul className={style.list}>
+                <ul className={listTheme}>
                     {searchInput && searchedTrains.map(train =>
 
                         <li key={train.id} className={style.item} onClick={() => {
