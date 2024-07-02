35f150c5990c4ee564587777b3b8d41411f624ed
Julian-Louis <contact@julianlouis.fr>
2024-07-02 22:47:14 +0200

    Do not use next image component anymore

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home({ host }) {
             <main >
                 <Container style={{ marginTop: 50 }}>
                     <Flex align="center" direction={"column"}>
-                        <Image alt="SimRail logo" src={"/logos/logo_simrailapp.png"} width={258} height={82} />
+                        <img alt="SimRail logo" src={"/logos/logo_simrailapp.png"} width={258} height={82} />
                         <Flex style={{ marginTop: 50 }} direction={"column"} gap={48}>
                             <FeatureCard name='Live Map' href='https://map.simrail.app' image='/features/map.png' />
                             <FeatureCard name='EDR' target="_blank" href='https://edr.simrail.app' image='/features/edr.png' />
