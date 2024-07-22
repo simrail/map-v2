e6ff09275680784de032432e9b77401a718ea2bd
Julian Louis <contact@julianlouis.fr>
2024-07-22 22:58:15 +0200

    Make mantine theme works

--- packages/map/contexts/ThemeContext.tsx
+++ packages/map/contexts/ThemeContext.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 
 interface Theme {
-    theme: string;
+    theme: 'light' | 'dark';
     setTheme: (theme: string) => void;
 }
 
--- packages/map/pages/_app.tsx
+++ packages/map/pages/_app.tsx
@@ -1,16 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { GoogleTagManager } from '@next/third-parties/google'
-import { ThemeProvider } from 'contexts/ThemeContext'
-import { colorsTuple, createTheme, MantineProvider, MantineThemeOverride } from '@mantine/core'
+import { ThemeProvider, useTheme } from 'contexts/ThemeContext'
+import { colorsTuple, createTheme, MantineProvider, MantineThemeOverride, useMantineColorScheme } from '@mantine/core'
 import '@mantine/core/styles.css';
 import '@mantine/spotlight/styles.css';
 import 'mantine-flagpack/styles.css';
 import '@mantine/carousel/styles.css';
+import { useEffect } from 'react'
 
 export default function Home({ Component, pageProps }: AppProps) {
 
+  return <>
+    {process.env.NODE_ENV !== 'development' && <GoogleTagManager gtmId="GTM-TWKQ769Q" />}
+    <ThemeProvider>
+      <Mantine>
+        <Component {...pageProps} />
+      </Mantine>
+      
+    </ThemeProvider>
+  </>
+}
 
+function Mantine({children}) {
   const mantineTheme: MantineThemeOverride = createTheme({
     headings: { fontFamily: 'Saira' },
     colors: {
@@ -20,12 +32,9 @@ export default function Home({ Component, pageProps }: AppProps) {
     }
   })
 
-  return <>
-    {process.env.NODE_ENV !== 'development' && <GoogleTagManager gtmId="GTM-TWKQ769Q" />}
-    <ThemeProvider>
-      <MantineProvider theme={mantineTheme} defaultColorScheme="dark">
-        <Component {...pageProps} />
-      </MantineProvider>
-    </ThemeProvider>
-  </>
+  const { theme } = useTheme();
+
+  return <MantineProvider theme={mantineTheme}    forceColorScheme={theme}>
+    {children}
+  </MantineProvider>
 }
