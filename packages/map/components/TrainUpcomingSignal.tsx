3a459b6fb28fd0498ab4a66f64f61f73853e5276
Loo Dessart <dessart.loo@orange.fr>
2024-07-25 13:23:14 +0200

    Next Signal update after interface update

--- packages/map/components/TrainUpcomingSignal.tsx
+++ packages/map/components/TrainUpcomingSignal.tsx
@@ -1,43 +1,104 @@
-import {Train} from "@simrail/types";
+import { Train } from "@simrail/types";
 import React from "react";
 
 type TrainSignalProps = {
-    train: Train;
+  train: Train;
+};
+
+const signalStates = {
+  open: "/signals/signal-open.png",
+  limited40: "/signals/signal-limited-40.png",
+  limited60: "/signals/signal-limited-60.png",
+  limited100: "/signals/signal-limited-100.png",
+  limited130: "/signals/signal-limited-130.png",
+  closed: "/signals/signal-closed.png",
 };
 
 function formatSignalDistance(distanceMeters: number): string {
-    if (distanceMeters > 1000) {
-        // signal is more than 1 kilometer away, display in kilometers
-        const distanceKilometers = (distanceMeters / 1000).toFixed(1);
-        return `${distanceKilometers} km`;
-    } else {
-        // closer than 1 kilometer, display in kilometers
-        return `${distanceMeters.toFixed(1)} m`;
-    }
+  if (distanceMeters > 5000) {
+    return "> 5km";
+  } else if (distanceMeters > 1000) {
+    const distanceKilometers = (distanceMeters / 1000).toFixed(1);
+    return `${distanceKilometers} km`;
+  } else {
+    return `${distanceMeters.toFixed(1)} m`;
+  }
 }
 
 function formatSignalSpeed(rawSpeedLimit: number): string {
-    if (rawSpeedLimit === 32767) {
-        return "vmax";
-    } else {
-        return `${rawSpeedLimit} km/h`;
-    }
+  if (rawSpeedLimit === 32767) {
+    return "vmax";
+  } else {
+    return `${rawSpeedLimit} km/h`;
+  }
 }
 
-const TrainUpcomingSignal: React.FC<TrainSignalProps> = ({train}) => {
-    const {TrainData: {SignalInFront, SignalInFrontSpeed, DistanceToSignalInFront}} = train;
+const getSignalState = (signalSpeed: number | string): string | null => {
+  if (signalSpeed === "vmax" || signalSpeed === 32767) {
+    return "open";
+  }
 
-    // signal names contain some sort of identifier delimited with a '@' which is
-    // completely irrelevant to the user so just remove it: 'KMB_M@-10828,8-1721,6'
-    const signalName = SignalInFront?.split("@")[0];
+  if (signalSpeed === 0) {
+    return "closed";
+  }
 
-    return (
+  if (typeof signalSpeed === "number" && signalSpeed <= 40) {
+    return "limited40";
+  }
+
+  if (typeof signalSpeed === "number" && signalSpeed <= 60) {
+    return "limited60";
+  }
+
+  if (typeof signalSpeed === "number" && signalSpeed <= 100) {
+    return "limited100";
+  }
+
+  if (typeof signalSpeed === "number" && signalSpeed <= 130) {
+    return "limited130";
+  }
+
+  return null;
+};
+
+const TrainUpcomingSignal: React.FC<TrainSignalProps> = ({ train }) => {
+  const {
+    TrainData: { SignalInFront, SignalInFrontSpeed, DistanceToSignalInFront },
+  } = train;
+
+  const signalName = SignalInFront?.split("@")[0];
+  const signalState = getSignalState(SignalInFrontSpeed);
+  const signalImageSrc = signalState
+    ? signalStates[signalState as keyof typeof signalStates]
+    : null;
+
+  return (
+    <>
+      <div>
+        Distance to {signalName ? signalName : "next signal"}:{" "}
+        {SignalInFront
+          ? formatSignalDistance(DistanceToSignalInFront)
+          : "Signal too far away"}
+      </div>
+      {SignalInFront && (
         <>
-            {signalName && <>Distance to: {formatSignalDistance(DistanceToSignalInFront)}</>}
-            {!signalName && <>Distance to: {">5km"}</>}
-            {signalName && <><br/>Signal speed: {formatSignalSpeed(SignalInFrontSpeed)}</>}
+          <div>Signal speed: {formatSignalSpeed(SignalInFrontSpeed)}</div>
+          {signalImageSrc && (
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <span>Signal Status : </span>
+              <img
+                src={signalImageSrc}
+                alt={signalState || ""}
+                width={35}
+                height={35}
+                style={{ marginLeft: "0.5rem" }}
+              />
+            </div>
+          )}
         </>
-    );
+      )}
+    </>
+  );
 };
 
 export default TrainUpcomingSignal;
